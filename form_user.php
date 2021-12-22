1f67536fde2269cf2bf4c71dc4529f7b8929b643
Gilang Mukharoom <55473537+gilangmukharom@users.noreply.github.com>
2021-12-22 12:06:12 +0700

    Add files via upload

--- form_user.php
+++ form_user.php
@@ -2,10 +2,10 @@
 echo "<h2>Tambah User</h2>
 <form method=post action=input_user.php>
 <table>
-<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
-<tr><td>Password</td><td> : <input name='password' type='password'></td></tr>
-<tr><td>Nama Lengkap</td><td> : <input name='nama' type='text'></td></tr>
-<tr><td>Email </td><td> : <input name='email' type='text'></td></tr>
+<tr><td>Username</td><td> : <input id='id_user' name='id_user' type='text'></td></tr>
+<tr><td>Password</td><td> : <input id='password' name='password' type='password'></td></tr>
+<tr><td>Nama Lengkap</td><td> : <input id='nama' name='nama' type='text'></td></tr>
+<tr><td>Email </td><td> : <input id='email' name='email' type='text'></td></tr>
 <tr><td colspan=2><input type='submit' value='SIMPAN'></td></tr>
 </table>
 </form>";
